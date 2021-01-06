3318421dfec14b6cbde65804df4ce9eeca4fd521
SolarPH <49668966+SolarPH@users.noreply.github.com>
2021-01-06 16:48:20 +0800

    Update c4d_ChildControlSnippet.py

--- c4d_ChildControlSnippet.py
+++ c4d_ChildControlSnippet.py
@@ -1,3 +1,7 @@
+# par = The First child of Parent Object
+# olen = Distance of child from Parent
+# co = The returned reference
+
 def getInstance(par,olen):
     co = par
     for x in range(olen):
