7b64c5824c5f1aab622c06566a948feae8248ae4
SolarPH <49668966+SolarPH@users.noreply.github.com>
2021-01-06 16:50:09 +0800

    Create c4d_AntiNoneSnippet.py

---
+++ c4d_AntiNoneSnippet.py
@@ -0,0 +1,20 @@
+def main():
+    # par = Link Port of Parent
+    # Next = Distance Count
+    # co = Present Instance
+    
+    if par != None:
+        Next = XPressoG[c4d.ID_USERDATA,2]
+        co = getInstance(par.GetDown(),Next)
+        if co != None:
+            co[c4d.ID_BASEOBJECT_REL_POSITION] = c4d.Vector(PX,PY,PZ)
+            co[c4d.ID_BASEOBJECT_REL_SCALE] = c4d.Vector(SX,SY,SZ)
+            co[c4d.ID_BASEOBJECT_REL_ROTATION] = c4d.Vector(RH,RP,RB)
+            XPressoG.SetName(co.GetName())
+            XPressoG[c4d.ID_USERDATA,3] = co
+        else:
+            XPressoG.SetName("Object")
+            XPressoG[c4d.ID_USERDATA,3] = None
+    else:
+        XPressoG.SetName("Object")
+        XPressoG[c4d.ID_USERDATA,3] = None
